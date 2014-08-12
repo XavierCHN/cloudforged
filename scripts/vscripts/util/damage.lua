5900d0a8c1af9ba4a142b943d8c47bdc9aaaa5aa
最萌小汐 <sunyijiusyj@126.com>
2014-08-12 13:39:01 +0800

    更新一下伤害系统模板

--- scripts/vscripts/util/damage.lua
+++ scripts/vscripts/util/damage.lua
@@ -9,6 +9,7 @@
 		"Function"		"Damage"
 		"damage"		"100"
 		"damage_type"	"DAMAGE_TYPE_PURE"
+		"damage_para"	"1"
 	}
 	
 	--]]
@@ -22,12 +23,12 @@
 
 	Damage.damage_meta = {
 		__index = {
-			attacker = nil, --伤害来源
-			victim = nil, --伤害目标
-			damage = 0, --默认伤害为0
-			damage_type = DAMAGE_TYPE_PURE, --默认伤害类型为纯粹
-			damage_flags = 1, --默认标记为1
-			para = 1, --伤害系数
+			attacker 		= nil, --伤害来源
+			victim 			= nil, --伤害目标
+			damage 			= 0, --默认伤害为0
+			damage_type		= DAMAGE_TYPE_PURE, --默认伤害类型为纯粹
+			damage_flags 	= 1, --默认标记为1
+			damage_para 	= nil, --伤害系数
 		},
 	}
 
@@ -36,9 +37,13 @@
 		--添加默认值
 		setmetatable(damage, Damage.damage_meta)
 
-		--按照公式计算伤害--（1+宁雨/10)*剑术*智力*1.2*等级/敌人等级 
-		local attacker = damage.attacker
-		damage = damage + (1 + attacker:getPara(1) / 10) * attacker:getPara(2) * attacker:getPara(3) * damage.para * attacker:getLevel() / damage.victim:getlevel()
+		--如果有伤害系数,则按照公式进行伤害计算;否则直接造成伤害
+		if damage.damage_para then
+			--按照公式计算伤害--（1+宁雨/10)*剑术*智力*1.2*等级/敌人等级 getPara函数是口胡的,用于获取宁雨/剑术/智力等参数
+			local attacker	= damage.attacker
+			local victim	= damage.victim
+			damage = damage + (1 + attacker:getPara(1) / 10) * attacker:getPara(2) * attacker:getPara(3) * damage.damage_para * attacker:GetLevel() / victim:GetLevel()
+		end
 		
 		--计算完毕
 		ApplyDamage(damage)
