0dd1388c62d6078e9a27fed7b66605edba36082e
最萌小汐 <sunyijiusyj@126.com>
2014-08-12 16:34:43 +0800

    更新伤害系统模板与说明

--- scripts/vscripts/util/damage.lua
+++ scripts/vscripts/util/damage.lua
@@ -7,10 +7,14 @@
 	{
 		"ScriptFile"	"scripts/vscripts/util/damage.lua"
 		"Function"		"Damage"
-		"damage"		"100"
+		"damage"		"0"
 		"damage_type"	"DAMAGE_TYPE_PURE"
-		"damage_para"	"1"
+		"damage_para"	"1.5"
 	}
+
+		damage		基础伤害,不写的话是0
+		damage_type	伤害类型,不写的话是纯粹伤害
+		damage_para	伤害系数,只有技能伤害需要用到,写了以后会按照默认的技能公式进行运算.非技能伤害不要填写
 	
 	--]]
 
@@ -23,12 +27,12 @@
 
 	Damage.damage_meta = {
 		__index = {
-			attacker 		= nil, --伤害来源
-			victim 			= nil, --伤害目标
-			damage 			= 0, --默认伤害为0
+			attacker 		= nil, 				--伤害来源
+			victim 			= nil, 				--伤害目标
+			damage 			= 0,				--默认伤害为0
 			damage_type		= DAMAGE_TYPE_PURE, --默认伤害类型为纯粹
-			damage_flags 	= 1, --默认标记为1
-			damage_para 	= nil, --伤害系数
+			damage_flags 	= 1, 				--默认标记为1
+			damage_para 	= nil, 				--伤害系数
 		},
 	}
 
