37662edd7de0df03425d0bd0c78b9396e2327d21
最萌小汐 <sunyijiusyj@126.com>
2014-08-20 18:15:30 +0800

    伤害系统会判定伤害目标是否合法

--- scripts/vscripts/util/damage.lua
+++ scripts/vscripts/util/damage.lua
@@ -70,11 +70,11 @@
 		setmetatable(damage, Damage.damage_meta)
 		
 		--获取技能传参,构建伤害table
-		damage.attacker		= damage.attacker or EntIndexToHScript(damage.caster_entindex)							--伤害来源(施法者)
+		damage.attacker			= damage.attacker or EntIndexToHScript(damage.caster_entindex)							--伤害来源(施法者)
 		damage.attacker_level	= damage.attacker:GetLevel()															--技能施放者的等级
 		damage.ability_level	= damage.ability:GetLevel()																--技能等级
 		damage.category_level	= ItemCore:GetAttribute(damage.attacker,damage.damage_category)							--伤害分类精通
-		damage.damage_type	= type(damage.damage_type) == 'string' and _G[damage.damage_type] or damage.damage_type	--转换伤害类型常量
+		damage.damage_type		= type(damage.damage_type) == 'string' and _G[damage.damage_type] or damage.damage_type	--转换伤害类型常量
 		
 
 		-- 伤害计算公式：
@@ -86,25 +86,28 @@
 		
 		
 		damage.damage_result=	damage.damage_base +
-								( damage.category_level
+								damage.category_level
 								*	damage.damage_increase
 								*	
-								(1 +
-									(
-									damage.attacker:GetStrength()	* damage.damage_str
-									+	damage.attacker:GetAgility()	* damage.damage_agi
-									+	damage.attacker:GetIntellect()	* damage.damage_int
+									(1 +
+										(
+											damage.attacker:GetStrength()	* damage.damage_str
+										+	damage.attacker:GetAgility()	* damage.damage_agi
+										+	damage.attacker:GetIntellect()	* damage.damage_int
+										)
 									)
-								)
 								*	damage.ability_level * damage.ability_level
-								*	damage.attacker_level)
+								*	damage.attacker_level
 
 		--遍历数组进行伤害		
 		for i, victim in ipairs(targets) do
-	        	damage.victim 		= victim
-	        	damage.victim_level	= victim:GetLevel()
-			damage.damage 		= math.max(damage.damage_min, damage.damage_result / damage.victim_level )
-			ApplyDamage(damage)
+				if victim:IsAlive() and victim:IsValidEntity() then
+		        	damage.victim 		= victim
+		        	damage.victim_level	= victim:GetLevel()
+					damage.damage 		= math.max(damage.damage_min, damage.damage_result / damage.victim_level)
+
+					ApplyDamage(damage)
+				end
 		end
 
 	end
