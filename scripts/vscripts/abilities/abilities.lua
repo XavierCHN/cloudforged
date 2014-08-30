37836a4e55025f396f236fc4f178f69217a8846e
a348730140 <a348730140@163.com>
2014-08-30 16:24:52 +0800

    上传拉比克召唤技能，未完成

--- scripts/vscripts/abilities/abilities.lua
+++ scripts/vscripts/abilities/abilities.lua
@@ -58,7 +58,7 @@ function rubick_sacrifice_off( keys )
 	caster:RemoveModifierByName("create_rubick_sacrifice_effect")
 end
 
---隐修议员 3技能 被动
+--隐修议员 4技能 被动
 function rubick_wise( keys )
 	local caster = EntIndexToHScript(keys.caster_entindex)
 	local ability=keys.ability
@@ -111,7 +111,7 @@ end
 
 
 
---隐修议员 4技能
+--隐修议员 大招
 --全局变量
 rubick_natural_shelter_channel=false
 --
@@ -319,5 +319,42 @@ function centaur_trample_road( keys )
 			EmitSoundOn("Hero_Centaur.Stampede.Cast", caster) 
 			return nil
 		end, 1)
+
+end
+
+--拉比克 3技能 召唤
+hRubickUnit=nil
+function rubick_defend( keys )
+	
+	if hRubickUnit~=nil then
+		hRubickUnit:RemoveSelf() 
+	end
+
+	local caster = keys.caster
+	local vec = caster:GetOrigin() + 200*caster:GetForwardVector()
+	hRubickUnit = CreateUnitByName("npc_dota_rubick_defend", vec, false, nil, nil, caster:GetTeam())
+	hRubickUnit.vOwner=caster:GetOwner()
+	hRubickUnit:SetControllableByPlayer(caster:GetPlayerID(), true)
+	hRubickUnit:SetForwardVector((hRubickUnit:GetOrigin() - caster:GetOrigin()):Normalized())
+	local ability = keys.ability
+	local i = ability:GetLevel()-1
+
+	--获取最小攻击=施法者最小攻击+施法者的智力*智力加成系数
+	local BaseDamageMin=caster:GetBaseDamageMin() + caster:GetIntellect() * ability:GetLevelSpecialValueFor("int", i)
+	--获取最大攻击=最小攻击+5
+	local BaseDamageMax=BaseDamageMin + 5
+
+	hRubickUnit:SetBaseDamageMin(BaseDamageMin)
+	hRubickUnit:SetBaseDamageMax(BaseDamageMax)	
+
+	--获取护甲=施法者护甲+施法者敏捷*敏捷加成系数
+	local armor = caster:GetPhysicalArmorValue() + caster:GetAgility() * ability:GetLevelSpecialValueFor("agi", i)+5
+	hRubickUnit:SetPhysicalArmorBaseValue(armor)
+
+	--获取生命值=1000 + 施法者的力量*力量加成系数
+	local health = 1000 + caster:GetStrength() * ability:GetLevelSpecialValueFor("str", i)
+	hRubickUnit:SetMaxHealth(health)
+	hRubickUnit:SetHealth(health)
+
 
 end
--- scripts/vscripts/quest/questcore.lua
+++ scripts/vscripts/quest/questcore.lua
@@ -3,7 +3,7 @@
 local MAX_QUEST_LIMIT = 5
 
 if QuestCore == nil then
-	QuestCore == class({})
+	QuestCore = class({})
 end
 
 function QuestCore:Init()
@@ -82,4 +82,45 @@ function QuestCore:GiveQuestRefund(playerid, questid)
 			-- TODO 给予物品
 		end
 	end
+end
+
+function QuestTest(keys)
+    print(keys.caller:GetName())-- 就是实体名字
+    print(keys.activator:GetUnitName()) -- 就是英雄名字，npc_hero_crystal_maiden什么的
+
+    local trigger_quest_name = keys.caller:GetName()
+    local trigger_hero = keys.activator
+    local playerid = trigger_hero:GetPlayerID()
+
+    local quest_name = "#QuestName_"..trigger_quest_name
+    local quest_desc = "#QuestDesc_"..trigger_quest_name
+    print("======QUEST TRIGGERED========")
+    print (quest_name)
+    print (quest_desc)
+    print("======QUEST TRIGGERED========")
+
+    FireGameEvent("quest_show", {
+    PlayerID = playerid,
+    QuestName = quest_name,
+    QuestDescription = quest_desc})
+
+    print("======GAME EVENT FIRED========")
+    --[[
+    ListenToGameEvent("player_accept_quest",
+    	function(keys)
+    		for k,v in pairs(keys) do
+    			print(k,v)
+    		end
+    	end, 
+    nil)]]
+    Convars:RegisterCommand('player_accept_quest',
+    	function(keys)
+
+    		print("accept quest clicked")
+    		print(keys)
+    		--[[
+    		for k,v in pairs(keys) do
+    			print(k,v)
+    		end]]
+    		end, "		", 0)
 end
